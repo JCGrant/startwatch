e87256c5ca765eea36e0bf40ea607e5bd826e446
JCGrant <james@jcgrant.com>
2025-04-05 17:13:23 +0100

    Initial commit

---
+++ src/lib/server/db/schema.ts
@@ -0,0 +1,17 @@
+import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const user = pgTable('user', {
+  id: serial('id').primaryKey(),
+});
+
+export const session = pgTable('session', {
+  id: text('id').primaryKey(),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => user.id),
+  expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull(),
+});
+
+export type Session = typeof session.$inferSelect;
+
+export type User = typeof user.$inferSelect;
